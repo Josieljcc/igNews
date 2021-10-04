1db68534bd30d97cff2bb1c1f51e96a683b56054
Josiel da Costa Carvalho <josiel.jcc@hotmail.com>
2021-10-04 18:14:39 -0300

    payment of subscription implemented

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,6 +5,7 @@ import { stripe } from "../../../services/stripe";
 export async function saveSubscription(
   subscriptionId: string,
   customerId: string,
+  createAction: boolean = false,
 ){
   //buscar usuario no falna com o customerId
   const userRef = await fauna.query(
@@ -29,11 +30,26 @@ export async function saveSubscription(
   }  
 
   // salvar os dados da subscription do usuario no fauna
-  await fauna.query(
+  if(createAction){await fauna.query(
     q.Create(
       q.Collection('subscriptions'),
       { data: subscriptionData }
     )
-  )
+  )}else{
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(
+            q.Match(
+              q.Index('subscription_by_id'),
+              subscriptionId,
+            )
+          )
+        ),
+        {data: subscriptionData}
+      )
+    )
+  }
 
 }
