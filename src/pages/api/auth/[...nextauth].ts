edb3e58e14988c116768b809463afe215637da5d
Josiel da Costa Carvalho <josiel.jcc@hotmail.com>
2021-10-06 00:54:19 -0300

    validate subscription

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth"
 import providers from "next-auth/providers"
 import {fauna} from '../../../services/fauna'
-import { query as q} from 'faunadb'
+import { Casefold, query as q} from 'faunadb'
+import { session } from "next-auth/client"
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -13,6 +14,42 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session){
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])
+          )
+        )
+        return {
+          ...session,
+          activeSubcription: userActiveSubscription
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubcription: null
+        }
+      }
+
+    },
     async signIn({ user, account, profile, email, credentials }) {
       try{
         await fauna.query(
